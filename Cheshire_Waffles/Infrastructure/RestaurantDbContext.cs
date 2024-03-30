6a574195de040b5756d42fc90c2d2b9099376ff8
Shahz87 <164032185+Shahz87@users.noreply.github.com>
2024-03-30 05:10:57 +0000

    Added: Infustrucure

---
+++ Cheshire_Waffles/Infrastructure/RestaurantDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Cheshire_Waffles.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cheshire_Waffles.Infrastructure
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
